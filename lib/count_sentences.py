2c7366ac9610fabebbdae22124f5777dd8bf44ea
Vallary Awuor Onyando <valonyando@gmail.com>
2025-07-01 13:28:48 +0300

    Passes tests

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
+import re
+
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, val):
+        if isinstance(val, str):
+            self._value = val
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        sentences = re.findall(r'[^.!?]+[.!?]', self.value)
+        return len(sentences)
+    
+val = MyString("Hi")
+print(val.is_question())
